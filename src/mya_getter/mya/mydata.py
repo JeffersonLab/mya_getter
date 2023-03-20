aff5938f989b02c906b635714e4b8b7f97e099a6
Adam Carpenter <adamc@jlab.org>
2023-03-20 16:01:21 -0400

    Added generic options to myData call

--- src/mya_getter/mya/mydata.py
+++ src/mya_getter/mya/mydata.py
@@ -22,15 +22,20 @@ class MyDataQuery(Query):
 
 
 # noinspection PyPep8Naming
-def myData(query: MyDataQuery, mydata_cmd: str = '/usr/csite/certified/bin/myData') -> pd.DataFrame:
+def myData(query: MyDataQuery, mydata_cmd: str = '/usr/csite/certified/bin/myData',
+           options: Optional[List[str]] = None) -> pd.DataFrame:
     """A wrapper on the myData command line application.
 
     Args:
         query: The object containing the query information.
         mydata_cmd: The path to the myData command
+        options: A list of command line options to be applied to the query other
+                 the the start, end, and PV list of the query.  Think deployment.
 """
 
     args = [mydata_cmd, '-b', query.begin, '-e', query.end, ] + query.pv_list
+    if options is not None:
+        args = args + options
     output = subprocess.run(args=args, check=True, capture_output=True)
     lines = output.stdout.decode('UTF-8').split('\n')
 
