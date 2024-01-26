899cb2c0e0fcf5cbcb52dbefce679d2d6a122453
Adam Carpenter <adamc@jlab.org>
2024-01-26 13:25:14 -0500

    Now import mySamplerWeb

--- src/mya_getter/mya/__init__.py
+++ src/mya_getter/mya/__init__.py
@@ -1,3 +1,3 @@
 from .mydata import MyDataQuery, myData
-from .mysampler import MySamplerQuery, mySampler
+from .mysampler import MySamplerQuery, mySampler, mySamplerWeb
 from ._mya import Query, do_parallel_queries
